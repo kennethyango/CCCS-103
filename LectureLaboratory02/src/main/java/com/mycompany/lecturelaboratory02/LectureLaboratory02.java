f623636481f5dba98da8890acb5c34a3b6004119
kennethyango <74163177+kennethyango@users.noreply.github.com>
2023-09-14 16:44:45 +0800

    as

--- LectureLaboratory02/src/main/java/com/mycompany/lecturelaboratory02/LectureLaboratory02.java
+++ LectureLaboratory02/src/main/java/com/mycompany/lecturelaboratory02/LectureLaboratory02.java
@@ -51,10 +51,65 @@ public class LectureLaboratory02 {
 //    System.out.println("Salary: " + salary);
 
 // Example #4 A program that converts the input weight in kg to lbs
-    Scanner myScanner = new Scanner(System.in);
-    System.out.println("Enter weight in kilograms: ");
-    float weight_kg = myScanner.nextFloat();
-    System.out.println("Your weight in pounds is " + weight_kg * 2.2f);
-    
+//    Scanner myScanner = new Scanner(System.in);
+//    System.out.println("Enter weight in kilograms: ");
+//    float weight_kg = myScanner.nextFloat();
+//    System.out.println("Your weight in pounds is " + weight_kg * 2.2f);
+//    
+
+
+
+// Example #5 Building a Basic Calculator
+//    Scanner myScanner = new Scanner(System.in);
+//    double num1, num2, sum, diff, prod, quo;
+//    System.out.println("Enter first number: ");
+//    num1 = myScanner.nextDouble();
+//    System.out.println("Enter second number: ");
+//    num2 = myScanner.nextDouble();
+//    sum = num1 + num2;
+//    System.out.println("The sum of the two numbers is: " +sum);
+
+// ====================================================================
+//
+//    Java has the following conditional statements:
+//
+//        Use if to specify a block of code to be executed, if a specified condition is true
+//        Use else to specify a block of code to be executed, if the same condition is false
+//        Use else if to specify a new condition to test, if the first condition is false
+//        Use switch to specify many alternative blocks of code to be executed
+//    
+//        IF CONDITION
+//        if (condition) {
+//           block of code to be executed if the condition is true
+//        }
+//        
+//        IF, ELSE CONDITION
+//        if (condition) {
+//          block of code to be executed if the condition is true
+//        } else {
+//          block of code to be executed if the condition is false
+//        }
+//            
+//        IF, ELSE IF CONDITION
+//        if (condition1) {
+//          // block of code to be executed if condition1 is true
+//        } else if (condition2) {
+//          // block of code to be executed if the condition1 is false and condition2 is true
+//        } else {
+//          // block of code to be executed if the condition1 is false and condition2 is false
+//        }   
+
+// Example #6
+//      Scanner myScanner = new Scanner(System.in);
+//      System.out.println("Enter a temperature in Celsius: ");
+//      double temp = myScanner.nextDouble();
+//      if (temp < 0) {
+//          System.out.println("ICE");
+//      } else if (temp >=0 && temp <=100){
+//          System.out.println("WATER");
+//      } else {
+//          System.out.println("STEAM");
+//      }
+        
     }
 }
