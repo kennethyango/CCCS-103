712277bb8c36ddf11c7c5e2bf3f9d4552e2df620
kennethyango <74163177+kennethyango@users.noreply.github.com>
2023-09-28 12:59:53 +0800

    MT

---
+++ MTLecLab101/src/main/java/com/mycompany/mtleclab101/MTLecLab101.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.mtleclab101;
+
+/**
+ *
+ * @author ISAAC
+ */
+public class MTLecLab101 {
+
+    public static void main(String[] args) {
+//        JAVA WHILE LOOP
+//The while loop loops through a block of code as long as a specified condition is true:
+// SYNTAX: 
+//        while (condition) {
+//          // code block to be executed
+//        }
+
+// Example #1
+
+
+    }
+}
