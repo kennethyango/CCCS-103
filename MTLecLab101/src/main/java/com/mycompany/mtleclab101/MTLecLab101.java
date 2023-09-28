59528a1dfcf39efc6a088abde81974839509e437
kennethyango <74163177+kennethyango@users.noreply.github.com>
2023-09-28 15:38:56 +0800

    09-28

--- MTLecLab101/src/main/java/com/mycompany/mtleclab101/MTLecLab101.java
+++ MTLecLab101/src/main/java/com/mycompany/mtleclab101/MTLecLab101.java
@@ -12,6 +12,59 @@ package com.mycompany.mtleclab101;
 public class MTLecLab101 {
 
     public static void main(String[] args) {
+//        Java Switch Statements
+// Instead of writing many if..else statements, you can use the switch statement.
+//        switch(expression) {
+//            case x:
+//              // code block
+//              break;
+//            case y:
+//              // code block
+//              break;
+//            default:
+//              // code block
+//         }
+// Switch Example #1:       
+// Create a program that returns the Day of a given number (1-7)        
+//            int day = 89;
+//            switch (day) {
+//                case 1:
+//                    System.out.println("Monday");
+//                    break;
+//                case 2:
+//                    System.out.println("Tuesday");
+//                    break;
+//                case 3:
+//                    System.out.println("Wednesday");
+//                    break;
+//                case 4:
+//                    System.out.println("Thursday");
+//                    break; 
+//                default:
+//                    System.out.println("Unknown Day");
+//                    break;
+//            }
+//
+//              char letter = 'r';
+//              switch(letter) {
+//                  case 'R':
+//                      System.out.println("RED");
+//                      break;
+//                  case 'r':
+//                      System.out.println("RED");
+//                      break;                      
+//                  case 'V':
+//                      System.out.println("VIOLET");
+//                      break;
+//                  case 'Y':
+//                      System.out.println("YELLOW");
+//                      break;                      
+//                  case 'P':
+//                      System.out.println("PURPLE");
+//                      break;                      
+//              }
+//// 
+        
 //        JAVA WHILE LOOP
 //The while loop loops through a block of code as long as a specified condition is true:
 // SYNTAX: 
@@ -20,7 +73,36 @@ public class MTLecLab101 {
 //        }
 
 // Example #1
+//            int i = 1;
+//            while (true) {
+//              System.out.println("The number is " + i);
+//              i++;  //increment i = i + 1
+//            }
 
+//          JAVA DO WHILE LOOP
+//        do {
+//          // code block to be executed
+//        } while (condition);
 
+// Example #2 DO WHILE LOOP
+//              int i = 1;
+//              do {
+//                 System.out.println("The number is " + i);
+//                 i++;
+//              } while (i<=10);\
+
+//      JAVA FOR LOOP
+//        Syntax:
+//            for (statement 1; statement 2; statement 3) {
+//              // code block to be executed
+//            }
+        //Statement 1 is executed (one time) before the execution of the code block.
+        //
+        //Statement 2 defines the condition for executing the code block.
+        //
+        //Statement 3 is executed (every time) after the code block has been executed.
+            for (int i = 100; i <= 115; i++) {
+              System.out.println("The number is " + i);
+            }
     }
 }
